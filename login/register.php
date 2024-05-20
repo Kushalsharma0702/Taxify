4ed393825a4406e87ecdb014e2aba0d8dfe71b4d
Kushal sharma <146510678+Kushalsharma0702@users.noreply.github.com>
2024-05-20 13:30:44 +0530

    commit

--- login/register.php
+++ login/register.php
@@ -1,91 +1,82 @@
 <?php
 require_once "config.php";
 
-
 if ($_SERVER['REQUEST_METHOD'] == "POST"){
 
-    // Check if username is empty
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Username cannot be blank";
+    // Initialize variables and error messages
+    $password_err = $confirm_password_err = $first_name_err = $last_name_err = $email_err = "";
+    $first_name = $last_name = $email = $password = "";
+
+    // Validate and sanitize First Name
+    if (empty(trim($_POST["FirstName"]))) {
+        $first_name_err = "First name cannot be blank";
+    } else {
+        $first_name = trim($_POST["FirstName"]);
+    }
+
+    // Validate and sanitize Last Name
+    if (empty(trim($_POST["LastName"]))) {
+        $last_name_err = "Last name cannot be blank";
+    } else {
+        $last_name = trim($_POST["LastName"]);
+    }
+
+    // Validate and sanitize Email
+    if (empty(trim($_POST["email"]))) {
+        $email_err = "Email cannot be blank";
+    } elseif (!filter_var(trim($_POST["email"]), FILTER_VALIDATE_EMAIL)) {
+        $email_err = "Invalid email format";
+    } else {
+        $email = trim($_POST["email"]);
+    }
+
+    // Check for password
+    if(empty(trim($_POST['password']))){
+        $password_err = "Password cannot be blank";
+    }
+    elseif(strlen(trim($_POST['password'])) < 5){
+        $password_err = "Password cannot be less than 5 characters";
     }
     else{
-        $sql = "SELECT id FROM users WHERE username = ?";
+        $password = trim($_POST['password']);
+    }
+
+    // Check for confirm password field
+    if(empty(trim($_POST['confirm_password']))){
+        $confirm_password_err = "Please confirm your password";
+    } elseif(trim($_POST['password']) !=  trim($_POST['confirm_password'])){
+        $confirm_password_err = "Passwords should match";
+    }
+
+    // If there were no errors, go ahead and insert into the database
+    if(empty($password_err) && empty($confirm_password_err) && empty($first_name_err) && empty($last_name_err) && empty($email_err))
+    {
+        $sql = "INSERT INTO users (password, first_name, last_name, email) VALUES (?, ?, ?, ?)";
         $stmt = mysqli_prepare($conn, $sql);
-        if($stmt)
+        if ($stmt)
         {
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            mysqli_stmt_bind_param($stmt, "ssss", $param_password, $param_first_name, $param_last_name, $param_email);
 
-            // Set the value of param username
-            $param_username = trim($_POST['username']);
+            // Set these parameters
+            $param_password = password_hash($password, PASSWORD_DEFAULT);
+            $param_first_name = $first_name;
+            $param_last_name = $last_name;
+            $param_email = $email;
 
-            // Try to execute this statement
-            if(mysqli_stmt_execute($stmt)){
-                mysqli_stmt_store_result($stmt);
-                if(mysqli_stmt_num_rows($stmt) == 1)
-                {
-                    $username_err = "This username is already taken"; 
-                }
-                else{
-                    $username = trim($_POST['username']);
-                }
+            // Try to execute the query
+            if (mysqli_stmt_execute($stmt))
+            {
+                header("location: login.php");
             }
             else{
-                echo "Something went wrong";
+                echo "Something went wrong... cannot redirect!";
             }
         }
+        mysqli_stmt_close($stmt);
     }
-
-    mysqli_stmt_close($stmt);
-
-
-// Check for password
-if(empty(trim($_POST['password']))){
-    $password_err = "Password cannot be blank";
+    mysqli_close($conn);
 }
-elseif(strlen(trim($_POST['password'])) < 5){
-    $password_err = "Password cannot be less than 5 characters";
-}
-else{
-    $password = trim($_POST['password']);
-}
-
-// Check for confirm password field
-if(trim($_POST['password']) !=  trim($_POST['confirm_password'])){
-    $password_err = "Passwords should match";
-}
-
-
-// If there were no errors, go ahead and insert into the database
-if(empty($username_err) && empty($password_err) && empty($confirm_password_err))
-{
-    $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-    $stmt = mysqli_prepare($conn, $sql);
-    if ($stmt)
-    {
-        mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
-
-        // Set these parameters
-        $param_username = $username;
-        $param_password = password_hash($password, PASSWORD_DEFAULT);
-
-        // Try to execute the query
-        if (mysqli_stmt_execute($stmt))
-        {
-            header("location: login.php");
-        }
-        else{
-            echo "Something went wrong... cannot redirect!";
-        }
-    }
-    mysqli_stmt_close($stmt);
-}
-mysqli_close($conn);
-}
-
 ?>
-
-
-
 
 <!doctype html>
 <html lang="en">
@@ -97,11 +88,11 @@ mysqli_close($conn);
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>PHP login system!</title>
+    <title>TAXIFY</title>
   </head>
   <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Php Login System</a>
+    <a class="navbar-brand" href="#">TAXIFY</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -148,14 +139,6 @@ mysqli_close($conn);
       <div class="form-group">
         <label for="inputConfirmPassword">Confirm Password</label>
         <input type="password" class="form-control" name="confirm_password" id="inputConfirmPassword" placeholder="Confirm Password">
-      </div>
-      <div class="form-group">
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" id="gridCheck">
-          <label class="form-check-label" for="gridCheck">
-            Check me out
-          </label>
-        </div>
       </div>
       <button type="submit" class="btn btn-primary">Sign in</button>
     </form>
